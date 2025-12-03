d3a56d3ae08a8bdd6ea0773d62562a3aa40e8629
AndreLimZK <s10267912@connect.np.edu.sg>
2025-12-03 16:13:18 +0800

    Made the Create in CRUD where users can make their accounts from the website.

--- register.js
+++ register.js
@@ -1,64 +1,38 @@
-// Import the functions you need from the SDKs you need
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-const firebaseConfig = {
-
-    apiKey: "AIzaSyCrJL5Wf2aYbJxraIil_Yz9TfTsimASnjs",
-
-    authDomain: "petsacrossworlds.firebaseapp.com",
-
-    databaseURL: "https://petsacrossworlds-default-rtdb.asia-southeast1.firebasedatabase.app",
-
-    projectId: "petsacrossworlds",
-
-    storageBucket: "petsacrossworlds.firebasestorage.app",
-
-    messagingSenderId: "247220418271",
-
-    appId: "1:247220418271:web:723622892e38f0e4ac9b6a",
-
-    measurementId: "G-YJXMNE9CY6"
-
-};
-
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js";
+import { firebaseConfig } from "./firebase-config.js";
 
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
 
+const auth = getAuth(app);
+const db = getDatabase(app);
+
 //submit button
-const submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', function (event) {
-    event.preventDefault();
+document.getElementById('signUpButton').addEventListener('click', (e) => {
+    e.preventDefault();
+    signUp();
 
     //inputs
-    const emailInput = document.getElementById('email').value;
-    const passwordInput = document.getElementById('password').value;
+    function signUp() {
+        const usernameInput = document.getElementById('username').value;
+        const emailInput = document.getElementById('email').value;
+        const passwordInput = document.getElementById('password').value;
 
-
-    createUserWithEmailAndPassword(auth, emailInput, passwordInput)
+        createUserWithEmailAndPassword(auth, emailInput, passwordInput)
         .then((userCredential) => {
-            // Signed up 
             const user = userCredential.user;
-            alert("Creating Account...")
-            // ...
+
+            set(ref(db, 'players/' + user.uid), {
+                username: usernameInput,
+            });
+
+            alert("Creating Account...");
         })
         .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert(errorMessage);
-            // ..
-        });
+            alert(error.message);
+        })
+    }
 });
